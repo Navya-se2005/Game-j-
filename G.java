42c3ca36672526f4be56e3b9964b65bdab495c19
Navya-se2005 <nvyashar1795@gmail.com>
2025-08-30 15:39:06 +0530

    Completed Game.java file

--- G.java
+++ G.java
@@ -7,5 +7,29 @@ public class G{
         Character enemy = new Mage("Enemy");
 
         System.out.println("Battle Start: "+ player.getName() + " vs " + enemy.getName());
+
+        while(player.isAlive() && enemy.isAlive()){
+            System.out.println("\nYour turn to choose action :  ");
+            System.out.println("1. Normal Attack");
+             System.out.println("2. Special Attack");
+             int choice = sc.nextInt();
+             if(choice == 1)
+             player.attack(enemy);
+             else if(choice == 2 && player instanceof Moves){
+                System.out.println("\n" + enemy.getName() + " is defeated! You Won!");
+                break;
+             }
+             if(Math.random()>0.5) 
+             enemy.attack(player);
+
+             else if(enemy instanceof Moves) 
+             ((Moves) enemy).specialAttack(player);
+                   if (!player.isAlive()) {
+                System.out.println("\n" + player.getName() + " is defeated! Game Over.");
+                break;
+            }
+        }
+        sc.close();
     }
+    
 }
